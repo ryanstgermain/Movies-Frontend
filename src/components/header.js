91502c0549703e9224cdc381343f2124e4bc4816
ryanstgermain <ryanjoel26@gmail.com>
2018-12-06 14:51:28 -0700

    added a header

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/App.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="font-colors">Ryan's Movie Ratings</h1>
+            <h3 className="font-colors">See if your opinions are correct!</h3>
+        </div>
+    );
+}
+
+export default Header;
