d5501de7004744681a9ee61c5b69fffc600b9d9b
ryanstgermain <ryanjoel26@gmail.com>
2019-01-02 12:52:44 -0700

    got the movies table to show up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import Header from './components/header.js';
+import NewMovie from './components/movie_button.js';
+import Movies from './components/movies.js';
 import './style/App.css';
 
 class App extends Component {
+  constructor(){
+      super ()
+        this.state = {
+            movies: [],
+            title: "",
+            description: "",
+            year: "",
+            rating: ""
+        }
+  }
+    componentDidMount() {
+        fetch('http://localhost:3000/')
+      .then((response) => response.json())
+      .then((data => this.setState({ movies: data })));
+  }
   render() {
     return (
       <div className="App">
         <Header />
+        <NewMovie />
+        <Movies movies={this.state.movies} />
       </div>
     );
   }
---
+++ src/components/Movie.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../style/App.css';
+import { Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
+
+const Movie = (props) => {
+    return (
+        <Table>
+            <thead className="table-head">
+                <tr className="table-head">
+                    <th>Title</th>
+                    <th>Director</th>
+                    <th>Year Made</th>
+                    <th>My Rating</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{props.cats.title}</td>
+                    <td>{props.cats.director}</td>
+                    <td>{props.cats.year}</td>
+                    <td>{props.cats.rating}</td>
+                    <Button bsStyle="default" className="table-buttons">Delete Movie</Button>
+                    <Button bsStyle="default" className="table-buttons">Edit</Button>
+                </tr>
+            </tbody>
+        </Table>
+    )
+}
+
+export default Movie;
